e975f06cfc4ad5ebcf4f2200be367cd42556f920
Huzefa Painter <painterhuzefa@gmail.com>
2026-04-10 17:04:07 +0530

    Fix: cors react url

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", "https://salary-tool-frontend.onrender.com"
+    origins "http://localhost:5173", "https://salary-tool-react.onrender.com"
 
     resource "*",
       headers: :any,
